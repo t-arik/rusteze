5bff0fa229e4d90b605354358bd52b76a81dc5cb
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-02-03 18:07:34 +0100

    chore: remove compiled and target dir

--- hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d
+++
@@ -1,5 +0,0 @@
-/informatik1/students/home/0badawy/Desktop/rusteze/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b: src/main.rs
-
-/informatik1/students/home/0badawy/Desktop/rusteze/hello_cargo/target/debug/deps/hello_cargo-4376c0aedfff852b.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/informatik1/students/home/0badawy/Desktop/rusteze/hello_cargo/target/debug/hello_cargo: /informatik1/students/home/0badawy/Desktop/rusteze/hello_cargo/src/main.rs
